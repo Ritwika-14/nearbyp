95e467c20c1fddbc8c416ab566839e8c4e08963c
Abhishek-465 <abhishekbhattacharjee12345@gmail.com>
2024-06-29 20:51:22 +0530

    version 1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './components/Home';
 
 const App = () => {
   return (
-    <div className=' bg-blue-50 dark:bg-gray-900 back overflow-y-hidden'>
+    <div className=' bg-blue-50 dark:bg-gray-900 back overflow-hidden'>
       <Navbar/>
    
       <Outlet/>
